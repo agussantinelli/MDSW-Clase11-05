6183e715b55737d60dc11e393dd2c51a1163b563
MartinUTN <martinratti10@gmail.com>
2026-05-11 20:49:24 -0300

    Test 2

--- string-calculator.js
+++ string-calculator.js
@@ -1,8 +1,9 @@
 function sumar(string) {
+  // test 1: string vacío retorna 0
+  if (string === '') return 0;
 
-  //test 1
-  return 0;
-
+  // test 2: un número retorna ese número
+  return parseInt(string);
 }
 
 module.exports = sumar;
--- string-calculator.test.js
+++ string-calculator.test.js
@@ -4,3 +4,9 @@ const sumar = require('./string-calculator');
 test('string vacío retorna 0', () => {
   expect(sumar('')).toBe(0);
 });
+
+// test 2
+test('un número retorna ese número', () => {
+  expect(sumar('1')).toBe(1);
+  expect(sumar('5')).toBe(5);
+});
