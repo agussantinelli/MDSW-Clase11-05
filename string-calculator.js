8e498296da8fd0c7ee87c90acdce069c60ad5b63
MartinUTN <martinratti10@gmail.com>
2026-05-11 21:49:48 -0300

    arreglo tets

--- string-calculator.js
+++ string-calculator.js
@@ -1,7 +1,14 @@
 function sumar(string) {
   if (string === '') return 0;
 
-  const numeros = string.split(/,|\n/);
+  let delimitador = /,|\n/;
+
+  if (string.startsWith('//')) {
+    delimitador = string[2];
+    string = string.substring(4);
+  }
+
+  const numeros = string.split(delimitador);
   let suma = 0;
   for (let num of numeros) {
     suma += parseInt(num);
